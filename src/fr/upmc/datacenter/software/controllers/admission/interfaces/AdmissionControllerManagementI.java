0d3609b44279d704fc9a16c2d711023eaea96249
Daniel RADEAU <d.radeau@gmail.com>
2017-01-22 15:44:27 +0100

    admission controller

--- src/fr/upmc/datacenter/software/controllers/admission/interfaces/AdmissionControllerManagementI.java
+++
@@ -1,5 +0,0 @@
-package fr.upmc.datacenter.software.controllers.admission.interfaces;
-
-public interface AdmissionControllerManagementI {
-
-}
--- src/fr/upmc/datacenter/software/controllers/admission/interfaces/AdmissionControllerServicesI.java
+++
@@ -1,8 +0,0 @@
-package fr.upmc.datacenter.software.controllers.admission.interfaces;
-
-public interface AdmissionControllerServicesI {
-
-	void submitApplication();
-	
-	void submitApplication(Class<?> interfaze);
-}
